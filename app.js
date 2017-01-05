2d008cd249a9d4ab17177757da8bb393523bb91a
scmitar1 <scmitar1@gmail.com>
2017-01-06 07:35:14 +0900

    개발툴 commit 및 개발 환경 변경

--- app.js
+++ app.js
@@ -10,15 +10,58 @@ Ext.application({
         'template.view.main.Main'
     ],
     extend: 'template.Application',
-    init: function() {
-        eui.Config.initLocaleMessage(function() {
-            Ext.create('Ext.container.Viewport', {
-                layout: 'fit',
-                items: [{
-                    xtype: 'app-main'
-                }]
-            })
+    defaultToken: '#home',
+    routes: {
+        'devel/:id': {
+            conditions: {
+                ':id': '[0-9a-zA-Z\.=?\&\_]+',
+            },
+            action: 'devel'
+        },
+        'home': 'home'
+    },
+
+    home: function () {
+        var mainView = this.getMainView && this.getMainView(),
+            viewport;
+        if (mainView) {
+            mainView.destroy();
+        }
+        this.setMainView('Ext.container.Viewport');
+        viewport = this.getMainView();
+        viewport.setLayout('fit');
+        eui.Config.initLocaleMessage(function () {
+            viewport.add({
+                xtype: 'app-main'
+            });
         });
-        //Util.globalCheckLogin('template');
-    }
+    },
+
+    devel: function () {
+        var clazz = location.hash.split('/')[1],
+            mainView = this.getMainView && this.getMainView(),
+            viewport;
+        if (mainView) {
+            mainView.destroy();
+        }
+        if (Ext.manifest.classes[clazz]) {
+            this.setMainView('Ext.container.Viewport');
+            viewport = this.getMainView();
+            viewport.setLayout('fit');
+            eui.Config.initLocaleMessage(function () {
+                viewport.add(Ext.create(clazz));
+            });
+        }
+    },
+    //
+    // init: function() {
+    //     eui.Config.initLocaleMessage(function() {
+    //         Ext.create('Ext.container.Viewport', {
+    //             layout: 'fit',
+    //             items: [{
+    //                 xtype: 'app-main'
+    //             }]
+    //         })
+    //     });
+    // }
 });
